4360965446e3a27b9fbc53b8ccd4ae251d73e545
Wojciech Dudczak <wojciech.dudczak@gpw.pl>
2020-02-05 15:36:51 +0100

    Area 51

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom'
+// import Parser from 'html-react-parser'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import HC_exporting from 'highcharts/modules/exporting'
@@ -7,91 +8,9 @@ import ColorScheme from 'highcharts/themes/grid'
 import { useTable } from 'react-table'
 
 import './App.css'
-import jsonData from './data/input.json'
-
-if (!jsonData.hasOwnProperty('title')) {
-  jsonData.title = 'Roundtrip percentiles'
-}
 
 ColorScheme(Highcharts);
 HC_exporting(Highcharts);
-
-var options = {
-  title: {
-    text: jsonData.title
-  },
-  legend: {
-    enabled: true,
-    align: 'left'
-  },
-  xAxis: {
-    gridLineWidth: 1,
-    type: 'logarithmic',
-    title: {
-      text: 'Percentile'
-    }
-  },
-  yAxis: {
-    tickInterval: 50,
-    minorTickInterval: 10,
-    max: 300,
-    title: {
-      text: 'Latency µs'
-    }
-  }
-}
-
-options.series = [];
-
-var tColumns = [{
-  Header: 'Summary', columns: [
-    {
-      Header: 'What',
-      accessor: 'what',
-    },
-    {
-      Header: 'Standard Deviation',
-      accessor: 'stdDev',
-    },
-    {
-      Header: 'Average [µs]',
-      accessor: 'mean',
-    },
-    {
-      Header: 'Median [µs]',
-      accessor: 'median',
-    },
-    {
-      Header: 'Min [µs]',
-      accessor: 'min',
-    },
-    {
-      Header: 'Max [µs]',
-      accessor: 'max',
-    },
-    {
-      Header: 'Count',
-      accessor: 'count',
-    },
-  ]
-}]
-
-var tableData = [];
-
-jsonData.chArray.forEach(function (entry) {
-  var td = entry.stats;
-
-  td.what = entry.name
-  tableData.push(td)
-
-  options.series.push({
-    'name': entry.name,
-    'data': entry.data
-  })
-})
-
-options.yAxis.max = jsonData.graphMax
-
 
 function Table({ columns, data }) {
   const {
@@ -134,20 +53,198 @@ function Table({ columns, data }) {
   )
 }
 
-const App = () => <div>
-  <div>
-    <HighchartsReact
-      highcharts={Highcharts}
-      options={options}
-    />
-  </div>
-  <div className='top10m'>
-    <Table
-      columns={tColumns}
-      data={tableData}
-    />
-  </div>
-</div>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    console.log(window.location.href)
+
+    this.state = { ok: 0 }
+
+    this.msg = '';
+
+    this.options = {
+      chart: {
+        height: 500,
+        width: 1800
+      },
+      legend: {
+        enabled: true,
+        align: 'left'
+      },
+      xAxis: {
+        tickInterval: 1,
+        tickPositions: [],
+        gridLineWidth: 1,
+        labels: {
+          format: '{value:.2f}',
+          rotation: -90
+        },
+        type: 'logarithmic',
+        title: {
+          text: 'Percentile'
+        }
+      },
+      yAxis: {
+        tickInterval: 50,
+        minorTickInterval: 10,
+        max: 100,
+        title: {
+          text: 'Latency µs'
+        }
+      },
+      series: []
+    }
+    this.histogramOptions = {
+      chart: {
+        type: 'column',
+        height: 800,
+        width: 1800
+      },
+      title: {
+        text: 'Histogram'
+      },
+      legend: {
+        enabled: true,
+        align: 'left'
+      },
+      // xAxis: {
+      //   tickInterval: 1,
+      //   tickPositions: [],
+      //   gridLineWidth: 1,
+      //   labels: {
+      //     format: '{value:.2f}',
+      //     rotation: -90
+      //   },
+      //   type: 'logarithmic',
+      //   title: {
+      //     text: 'Percentile'
+      //   }
+      // },
+      yAxis: {
+        tickInterval: 50,
+        minorTickInterval: 10,
+        title: {
+          text: 'Count'
+        }
+      },
+      series: []
+    }
+
+    this.tColumns = [
+      {
+        Header: 'What',
+        accessor: 'what',
+      },
+      {
+        Header: 'Standard Deviation',
+        accessor: 'stdDev',
+      },
+      {
+        Header: 'Average [µs]',
+        accessor: 'mean',
+      },
+      {
+        Header: 'Median [µs]',
+        accessor: 'median',
+      },
+      {
+        Header: 'Min [µs]',
+        accessor: 'min',
+      },
+      {
+        Header: 'Max [µs]',
+        accessor: 'max',
+      },
+      {
+        Header: 'Count',
+        accessor: 'count',
+      },
+    ]
+    this.tableData = [];
+  }
+
+  componentDidMount() {
+    var self = this
+
+    fetch('http://localhost:4000/setup')
+      .then(response => response.json())
+      .then(jsonData => {
+        self.options.title = { text: jsonData.title }
+
+        if (jsonData.hasOwnProperty('subTitle')) {
+          self.options.subtitle = { text: jsonData.subTitle }
+        }
+
+        if (jsonData.hasOwnProperty('graphMax')) {
+          self.options.yAxis.max = jsonData.graphMax
+        }
+
+        jsonData.chArray.forEach(function (entry) {
+          var td = entry.stats;
+
+          td.what = entry.name
+          self.tableData.push(td)
+
+          self.options.series.push({
+            'name': entry.name,
+            'data': entry.data
+          })
+
+          self.histogramOptions.series.push({
+            'name': entry.name,
+            'data': entry.histogram
+          })
+        })
+
+        jsonData.chArray[0].data.forEach(function (element) {
+          self.options.xAxis.tickPositions.push(Math.log10(element[0]))
+        })
+
+        self.setState({ ok: 1 })
+      })
+      .catch((err) => {
+        self.setState({ ok: 2 })
+        self.msg = '<p>err</p>'
+      })
+  }
+
+  render() {
+    if (this.state.ok === 0) {
+      return null
+    }
+
+    if (this.state.ok === 2) {
+      return (
+        <div>Error</div>
+      )
+    }
+
+    return (
+      <div>
+        <div>
+          <HighchartsReact
+            highcharts={Highcharts}
+            options={this.options}
+          />
+        </div>
+        <div className='top10m'>
+          <Table
+            columns={this.tColumns}
+            data={this.tableData}
+          />
+        </div>
+        <div className='top10m'>
+          <HighchartsReact
+            highcharts={Highcharts}
+            options={this.histogramOptions}
+          />
+        </div>
+
+      </div>
+    );
+  }
+}
 
 render(<App />, document.getElementById('root'))
 
